9417a86e6921117c7aee5731c6219130529918e4
Soutec Digital <soutecdigital@gmail.com>
2026-06-01 10:36:19 -0300

    Fix: Adiciona matricula ao seeder de users para passar na constraint
    Fix: Adiciona matricula ao seeder de users para passar na constraint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'Test User',
                 'perfil' => 'admin',
+                'matricula' => '123456',
                // 'ativo' => true,
                 'password' => \Illuminate\Support\Facades\Hash::make('password'), // Defina uma senha padrão
             ]
