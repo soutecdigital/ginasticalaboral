fe3d0da1302a0caf58d23b0f14fdbba5bb44d756
Soutec Digital <soutecdigital@gmail.com>
2026-06-01 08:41:35 -0300

    Altera para migrate comum para proteger os dados
    Altera para migrate comum para proteger os dados

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Rodar as migrations limpando o banco rascunho anterior
-php artisan migrate:fresh --force
+php artisan migrate --force
 
 # Iniciar o servidor do Apache em primeiro plano
 exec apache2-foreground
