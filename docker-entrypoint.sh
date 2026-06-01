fd73e828606e83e46e7d567a2ddf2f7fd350a5e2
Soutec Digital <soutecdigital@gmail.com>
2026-06-01 09:48:15 -0300

    PProd: Configuração definitiva de produção e remoção da rota de seed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# 1. Roda as novas migrations na produção
+# 1. Roda apenas as NOVAS migrations com segurança (NUNCA use fresh aqui em produção!)
 php artisan migrate --force
 
-# 2. Limpa todos os caches antigos que travam rotas novas
+# 2. Mantém os caches limpos para garantir que novas rotas e telas funcionem direto
 php artisan route:clear
 php artisan config:clear
 php artisan cache:clear
 
-# 3. Inicia o servidor Apache
+# 3. Inicia o servidor Apache em primeiro plano
 exec apache2-foreground
