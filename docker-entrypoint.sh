35b28b348e65adc4d0942b2b7181da89b8597201
Soutec Digital <soutecdigital@gmail.com>
2026-06-01 09:23:19 -0300

    Fix: limpa cache de rotas no entrypoint
    Fix: limpa cache de rotas no entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,10 @@
 # 1. Roda as novas migrations na produção
 php artisan migrate --force
 
-# 2. Força a execução dos Seeders mesmo em ambiente de produção
-php artisan db:seed --force
+# 2. Limpa todos os caches antigos que travam rotas novas
+php artisan route:clear
+php artisan config:clear
+php artisan cache:clear
 
 # 3. Inicia o servidor Apache
 exec apache2-foreground
