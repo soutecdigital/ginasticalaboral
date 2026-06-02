50247c3e6e84ce7306caf6d79346c078e4a0c9e7
Soutec Digital <soutecdigital@gmail.com>
2026-06-02 14:05:28 -0300

    Improve comments in docker-entrypoint.sh
    Updated comments for clarity regarding database migration and cache clearing.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Força a limpeza e criação de todas as tabelas estruturais de uma vez
-php artisan migrate:fresh  --force
+# Força o reset total do banco estrutural para limpar o Neon
+php artisan migrate:fresh --force
 
-# Limpa os caches
+# Limpa os caches de produção
 php artisan route:clear
 php artisan config:clear
 php artisan cache:clear
