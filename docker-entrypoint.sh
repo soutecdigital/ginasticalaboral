225df237af7e93421ac5d86a324e738fbced5f78
Soutec Digital <soutecdigital@gmail.com>
2026-06-02 12:59:34 -0300

    Modify migration command and enhance cache comments
    Updated migration command to run safely without data loss and improved cache clearing comments.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# Roda apenas as novas migrações e os seeds sem apagar os dados existentes
-php artisan migrate:fresh --seed --force
+# Poka-Yoke: Executa apenas as novas migrações com segurança, sem apagar dados existentes
+php artisan migrate --force
 
-# Limpa os caches
+# Limpa e otimiza os caches para o ambiente produtivo
 php artisan route:clear
 php artisan config:clear
 php artisan cache:clear
+php artisan view:clear
 
 exec apache2-foreground
